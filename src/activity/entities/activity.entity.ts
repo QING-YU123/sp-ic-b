97d08c75b67294911983ae9d90e0c5674f09d84a
liyucan001 <1575749939@qq.com>
2024-06-13 16:07:13 +0800

    6-13调整

--- src/activity/entities/activity.entity.ts
+++ src/activity/entities/activity.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class Activity {
+export class ActivityC {
     @PrimaryGeneratedColumn({ type: "bigint" })
     id: number;
 
