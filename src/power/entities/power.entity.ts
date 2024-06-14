d5c3268f9142ee595be257ce68394ace39b8884e
liyucan001 <1575749939@qq.com>
2024-06-14 15:12:48 +0800

    大改数据库

--- src/power/entities/power.entity.ts
+++ src/power/entities/power.entity.ts
@@ -5,66 +5,66 @@ export class Power {
     @PrimaryColumn({ type: "int" })
     id: number;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_admin0", type: "boolean", default: false })
     mAdmin0: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_admin1", type: "boolean", default: false })
     mAdmin1: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_user", type: "boolean", default: false })
     mUser: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_payment", type: "boolean", default: false })
     mPayment: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_feedback", type: "boolean", default: false })
     mFeedback: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_notice", type: "boolean", default: false })
     mNotice: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_approve", type: "boolean", default: false })
     mApprove: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_ban_talk", type: "boolean", default: false })
     mBanTalk: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_activity", type: "boolean", default: false })
     mActivity: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_store_approve", type: "boolean", default: false })
     mStoreApprove: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_store", type: "boolean", default: false })
     mStore: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_repair", type: "boolean", default: false })
     mRepair: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_outsider", type: "boolean", default: false })
     mOutsider: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "m_parking", type: "boolean", default: false })
     mParking: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "u_approve", type: "boolean", default: false })
     uApprove: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "u_collect", type: "boolean", default: false })
     uCollect: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "u_money", type: "boolean", default: false })
     uMoney: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "u_repair", type: "boolean", default: false })
     uRepair: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "u_feedback", type: "boolean", default: false })
     uFeedback: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "u_activity", type: "boolean", default: false })
     uActivity: boolean;
 
-    @Column({ type: "boolean", default: false })
+    @Column({ name: "u_notice", type: "boolean", default: false })
     uNotice: boolean;
 }
